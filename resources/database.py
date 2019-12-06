2e12a0c8a88782c15968d7470ed89e28205130c7
Giacomo986 <giacomo.perin@gmail.com>
2019-12-06 22:50:59 +0100

    a

--- resources/database.py
+++ resources/database.py
@@ -3,4 +3,22 @@ import mysql.connector as mariadb
 mariadb_connection = mariadb.connect(user="freecad", password="freecad", database="dbpezzi")
 cursor = mariadb_connection.cursor()
 
-cursor.execute("CREATE TABLE roasts (id INTEGER PRIMARY KEY, description TEXT NOT NULL UNIQUE, color TEXT NOT NULL UNIQUE);")
+def Crea_tabella():
+    cursor.execute( "CREATE TABLE IF NOT EXISTS tubi ("
+                    "   task_id INT AUTO_INCREMENT PRIMARY KEY,"
+                    "   riferimento VARCHAR(255) NOT NULL,"
+                    "   codice_padre TEXT,"
+                    "   macchina TEXT,"
+                    "   materiale TEXT,"
+                    "   denominazione_profilo TEXT,"
+                    "   data_creazione DATE,"
+                    "   nome TEXT,"
+                    "   codice TEXT,"
+                    "   quantità_per_disegno INT,"
+                    "   misura_di_massima TEXT,"
+                    "   massa INT,"
+                    "   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+                    ")  ENGINE=INNODB;")
+
+def inserisci_riga();
+    pass
