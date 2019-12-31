72ff09502e15bfcf4031fdd425d148bee48135c2
Giacomo986 <giacomo.perin@gmail.com>
2019-12-31 15:06:59 +0100

    Calcolo massa automatico

--- resources/interfaccia.py
+++ resources/interfaccia.py
@@ -12,101 +12,108 @@ from PySide2 import QtCore, QtGui, QtWidgets
 
 class Ui_Form(object):
     def setupUi(self, Form):
-        Form.setObjectName("Form")
+        
         Form.resize(600, 500)
+
+        reg_ex = QtCore.QRegExp(r"[-+]?[0-9]*\.?[0-9]+") # Regular Expression per filtrare solo valori numerici dove necessario
+
         self.gridLayout = QtWidgets.QGridLayout(Form)
-        self.gridLayout.setObjectName("gridLayout")
+        
         self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setObjectName("label_4")
         self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+
         self.label_12 = QtWidgets.QLabel(Form)
-        self.label_12.setObjectName("label_12")
         self.gridLayout.addWidget(self.label_12, 11, 0, 1, 1)
+
         self.label_7 = QtWidgets.QLabel(Form)
-        self.label_7.setObjectName("label_7")
         self.gridLayout.addWidget(self.label_7, 6, 0, 1, 1)
+
         self.label = QtWidgets.QLabel(Form)
-        self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
         self.lineEdit_Riferimento = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Riferimento.setObjectName("lineEdit_Riferimento")
         self.gridLayout.addWidget(self.lineEdit_Riferimento, 0, 6, 1, 1)
+
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setObjectName("label_2")
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
         self.lineEdit_CodicePadre = QtWidgets.QLineEdit(Form)
-        self.lineEdit_CodicePadre.setObjectName("lineEdit_CodicePadre")
         self.gridLayout.addWidget(self.lineEdit_CodicePadre, 1, 6, 1, 1)
+
         self.label_5 = QtWidgets.QLabel(Form)
-        self.label_5.setObjectName("label_5")
         self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
+
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setObjectName("label_3")
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
         self.lineEdit_Macchina = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Macchina.setObjectName("lineEdit_Macchina")
         self.gridLayout.addWidget(self.lineEdit_Macchina, 2, 6, 1, 1)
+
         self.comboBox_Denominazione = QtWidgets.QComboBox(Form)
-        self.comboBox_Denominazione.setObjectName("comboBox_Denominazione")
         self.gridLayout.addWidget(self.comboBox_Denominazione, 4, 6, 1, 1)
+
         self.lineEdit_Massa = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Massa.setObjectName("lineEdit_Massa")
+        Massa_input_validator = QtGui.QRegExpValidator(reg_ex, self.lineEdit_Massa)
+        self.lineEdit_Massa.setValidator(Massa_input_validator)
         self.gridLayout.addWidget(self.lineEdit_Massa, 11, 6, 1, 1)
+        
         self.lineEdit_Quantita = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Quantita.setObjectName("lineEdit_Quantita")
+        Quantita_input_validator = QtGui.QRegExpValidator(reg_ex, self.lineEdit_Massa)
+        self.lineEdit_Quantita.setValidator(Quantita_input_validator)
         self.gridLayout.addWidget(self.lineEdit_Quantita, 9, 6, 1, 1)
+
         self.lineEdit_Codice = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Codice.setObjectName("lineEdit_Codice")
         self.gridLayout.addWidget(self.lineEdit_Codice, 7, 6, 1, 1)
+
         self.comboBox_MisuraMax = QtWidgets.QComboBox(Form)
-        self.comboBox_MisuraMax.setObjectName("comboBox_MisuraMax")
         self.gridLayout.addWidget(self.comboBox_MisuraMax, 10, 6, 1, 1)
+
         self.comboBox_Cliente = QtWidgets.QComboBox(Form)
-        self.comboBox_Cliente.setObjectName("comboBox_Cliente")
         self.gridLayout.addWidget(self.comboBox_Cliente, 8, 6, 1, 1)
+
         self.comboBox_Materiale = QtWidgets.QComboBox(Form)
-        self.comboBox_Materiale.setObjectName("comboBox_Materiale")
         self.gridLayout.addWidget(self.comboBox_Materiale, 3, 6, 1, 1)
+
         self.label_8 = QtWidgets.QLabel(Form)
-        self.label_8.setObjectName("label_8")
         self.gridLayout.addWidget(self.label_8, 7, 0, 1, 1)
+
         self.label_9 = QtWidgets.QLabel(Form)
-        self.label_9.setObjectName("label_9")
         self.gridLayout.addWidget(self.label_9, 8, 0, 1, 1)
+
         self.lineEdit_Nome = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Nome.setObjectName("lineEdit_Nome")
         self.gridLayout.addWidget(self.lineEdit_Nome, 6, 6, 1, 1)
+
         self.label_6 = QtWidgets.QLabel(Form)
-        self.label_6.setObjectName("label_6")
         self.gridLayout.addWidget(self.label_6, 5, 0, 1, 1)
+
         self.label_10 = QtWidgets.QLabel(Form)
-        self.label_10.setObjectName("label_10")
         self.gridLayout.addWidget(self.label_10, 9, 0, 1, 1)
+
         self.label_11 = QtWidgets.QLabel(Form)
-        self.label_11.setObjectName("label_11")
         self.gridLayout.addWidget(self.label_11, 10, 0, 1, 1)
-        self.lineEdit_Data = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Data.setObjectName("lineEdit_Data")
-        self.gridLayout.addWidget(self.lineEdit_Data, 5, 6, 1, 1)
+
+        self.DateTimeEdit_Data = QtWidgets.QDateTimeEdit(Form)
+        self.gridLayout.addWidget(self.DateTimeEdit_Data, 5, 6, 1, 1)
+    
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setSpacing(6)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+
         self.CancelButton = QtWidgets.QPushButton(Form)
-        self.CancelButton.setObjectName("CancelButton")
         self.horizontalLayout.addWidget(self.CancelButton)
+
         self.AcceptButton = QtWidgets.QPushButton(Form)
-        self.AcceptButton.setObjectName("AcceptButton")
         self.horizontalLayout.addWidget(self.AcceptButton)
+
         self.gridLayout.addLayout(self.horizontalLayout, 12, 6, 1, 1)
 
         self.retranslateUi(Form)
-        #QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Salva Informazioni"))
         self.label_4.setText(_translate("Form", "Materiale:"))
-        self.label_12.setText(_translate("Form", "Massa:"))
+        self.label_12.setText(_translate("Form", "Massa (grammi):"))
         self.label_7.setText(_translate("Form", "Nome:"))
         self.label.setText(_translate("Form", "Riferimento:"))
         self.lineEdit_Riferimento.setToolTip(_translate("Form", "Codice disegno (testo libero)"))
