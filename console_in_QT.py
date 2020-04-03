2ed601e4a28556ca91b06c95832d5320584a36bd
Giacomo Perin <giacomo.perin@gmail.com>
2020-04-03 09:55:00 +0200

    a

--- console_in_QT.py
+++ console_in_QT.py
@@ -1,9 +1,22 @@
-import subprocess, sys
+import subprocess, sys, os, fcntl
 from PySide2 import QtWidgets, QtCore, QtGui, QtUiTools
+from subprocess import Popen, PIPE
+
+sub_proc = Popen("ping -c 10 localhost", stdout=PIPE, shell=True)
+sub_outp = ""
+
+def non_block_read(output):
+    ''' even in a thread, a normal read with block until the buffer is full '''
+    fd = output.fileno()
+    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+    try:
+        return output.read().decode("utf-8")
+    except:
+        return ''
 
 def Testo():
-    #window.textEdit.setText("aaaaaaaaaaaaaa")
-    window.textEdit.append(subprocess.check_output(['pip', 'install', "mysql-connector-python"]))
+    window.textEdit.append(non_block_read(sub_proc.stdout))
     pass
 
 if __name__ == "__main__":
